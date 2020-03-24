020681469b5dbebb6ae3a0ae21a71f4d47560c8a
eveporcello <eve@moonhighway.com>
2020-03-24 14:03:57 -0700

    adds complete version

--- index.js
+++ index.js
@@ -1,18 +1,72 @@
-const { ApolloServer, gql } = require("apollo-server");
+const {
+  ApolloServer,
+  gql,
+  PubSub
+} = require("apollo-server");
+
+const pubsub = new PubSub();
+
+let tabs = 0,
+  spaces = 0;
 
 const typeDefs = gql`
+  enum Style {
+    TABS
+    SPACES
+  }
+
+  type Results {
+    tabs: Int!
+    spaces: Int!
+  }
+
   type Query {
-    build: String!
+    results: Results!
+    totalVotes: Int!
+  }
+
+  type Mutation {
+    vote(style: Style!): String!
+  }
+
+  type Subscription {
+    result: Results!
   }
 `;
 
 const resolvers = {
   Query: {
-    build: () => "Build Something Cool!"
+    results: () => ({ tabs, spaces }),
+    totalVotes: () => tabs + spaces
+  },
+  Mutation: {
+    vote(parent, { style }, { pubsub }) {
+      const inc = 1;
+      if (style === "TABS") tabs += inc;
+      else spaces += inc;
+
+      pubsub.publish("vote-recorded", {
+        result: { tabs, spaces }
+      });
+
+      return "Thank you for voting!";
+    }
+  },
+  Subscription: {
+    result: {
+      subscribe: (parent, args, { pubsub }) =>
+        pubsub.asyncIterator("vote-recorded")
+    }
   }
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const context = { pubsub };
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context
+});
 
 server
   .listen()
